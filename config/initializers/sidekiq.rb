c02cca47b517db299e7eae7cdf0302f02b7ca370
Dave VanderKloot <dave@1530design.com>
2018-03-18 00:07:04 -0500

    update sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'], network_timeout: 10 }
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] || ENV['REDISTOGO_URL'], network_timeout: 10 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'], network_timeout: 10 }
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] || ENV['REDISTOGO_URL'], network_timeout: 10 }
 end
