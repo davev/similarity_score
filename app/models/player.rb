0aa6c7d95b4a2840b55d9ec0204ec147a949f922
Dave VanderKloot <dave@1530design.com>
2018-03-18 09:52:56 -0500

    add model for scraped players

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,7 @@ class Player < ApplicationRecord
   has_many :similar_career_players, -> { where(age:nil).order(score: :desc) }, class_name: 'SimilarPlayer', dependent: :destroy
   has_many :similar_age_players, -> { where.not(age:nil).order(age: :asc, score: :desc) }, class_name: 'SimilarPlayer', dependent: :destroy
 
+  scope :scraped, -> { where.not(active_year_begin: nil) }
 
   def scraped?
     active_year_begin.present?
