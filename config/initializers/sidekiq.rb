cffe265cf76e77687268f0f831d09c90b81aa4d1
Dave VanderKloot <dave@1530design.com>
2018-03-18 01:14:59 -0500

    testing sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,12 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: (ENV['SIDEKIQ_REDIS_URL'] || ENV['REDIS_URL']), network_timeout: 10 }
 
-  if defined?(ActiveRecord::Base)
-    config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = (ENV['DATABASE_REAPING_FREQUENCY'] || 10).to_i # seconds
-    ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord in Sidekiq')
-  end
+  # if defined?(ActiveRecord::Base)
+  #   config = Rails.application.config.database_configuration[Rails.env]
+  #   config['reaping_frequency'] = (ENV['DATABASE_REAPING_FREQUENCY'] || 10).to_i # seconds
+  #   ActiveRecord::Base.establish_connection
+  #   Rails.logger.info('Connected to ActiveRecord in Sidekiq')
+  # end
 
 end
 
