003aac1848ce0fc4edae0168caf0f7c0d7930111
Dave VanderKloot <dave@1530design.com>
2018-03-22 16:16:16 -0500

    refactor of player show page

--- app/assets/javascripts/clickables.js
+++ app/assets/javascripts/clickables.js
@@ -1,6 +1,7 @@
 $(document).on('click', '.clickable', function(event) {
   let url = $(this).data('url');
   if (url) {
-    window.location.assign(url);
+    Turbolinks.visit(url);
+    // window.location.assign(url); non-Turbolinks-way
   }
 });
