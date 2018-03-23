fb560e6851ed7dcc33f34378e4c74551de65f500
Dave VanderKloot <dave@1530design.com>
2018-03-22 22:42:27 -0500

    check that Turbolinks is defined and supported, otherwise gracefully handle with a fallback

--- app/assets/javascripts/clickables.js
+++ app/assets/javascripts/clickables.js
@@ -1,7 +1,10 @@
 $(document).on('click', '.clickable', function(event) {
   let url = $(this).data('url');
   if (url) {
-    Turbolinks.visit(url);
-    // window.location.assign(url); non-Turbolinks-way
+    if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+      Turbolinks.visit(url);
+    } else {
+      window.location.assign(url);
+    }
   }
 });
