16a1fbbc0016ef9f6bc27fc20e767214d31ac62c
Dave VanderKloot <dave@1530design.com>
2018-03-22 21:40:39 -0500

    fix n+1 issue, adjust cols to use new mediumplus breakpoint in player grids

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,8 +3,8 @@ class Player < ApplicationRecord
   friendly_id :name
 
   has_one :career_stat
-  has_many :similar_careers, -> { where(age: nil).order(score: :desc) }, class_name: 'SimilarPlayer', dependent: :destroy
-  has_many :similar_ages, -> { where.not(age: nil).order(age: :asc, score: :desc) }, class_name: 'SimilarPlayer', dependent: :destroy
+  has_many :similar_careers, -> { includes(:related_player).where(age: nil).order(score: :desc) }, class_name: 'SimilarPlayer', dependent: :destroy
+  has_many :similar_ages, -> { includes(:related_player).where.not(age: nil).order(age: :asc, score: :desc) }, class_name: 'SimilarPlayer', dependent: :destroy
 
   # has_many :similar_career_players, through: :similar_careers, source: :related_player
   # has_many :similar_age_players, through: :similar_ages, source: :related_player
@@ -17,11 +17,11 @@ class Player < ApplicationRecord
   end
 
   def similar_career_players
-    @similar_career_players ||= similar_careers.includes(:related_player).collect(&:related_player)
+    @similar_career_players ||= similar_careers.collect(&:related_player)
   end
 
   def similar_age_players
-    @similar_age_players ||= similar_ages.includes(:related_player).collect(&:related_player)
+    @similar_age_players ||= similar_ages.collect(&:related_player)
   end
 
   def scraped?
