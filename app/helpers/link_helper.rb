177f5a70330b25f70673c3726a2192ab205a079f
Dave VanderKloot <dave@1530design.com>
2018-03-23 16:08:04 -0500

    refactor links into helper and add about content

---
+++ app/helpers/link_helper.rb
@@ -0,0 +1,26 @@
+module LinkHelper
+
+  def link_to_baseball_reference_player(player, text = "view full stats")
+    return unless player
+
+    url = URI.join(ENV.fetch('BASEBALL_REFERENCE_URL'), player.handle).to_s
+    link_to text, url
+  end
+
+  def link_to_baseball_reference(text = 'baseball-reference.com')
+    link_to text, 'https://www.baseball-reference.com'
+  end
+
+  def link_to_similarity_scores_description(text = 'Similarity Scores')
+    link_to text, 'https://www.baseball-reference.com/about/similarity.shtml'
+  end
+
+  def link_to_bill_james(text = 'Bill James')
+    link_to text, 'https://en.wikipedia.org/wiki/Bill_James'
+  end
+
+  def link_to_trigram(text = 'trigram')
+    link_to text, 'https://about.gitlab.com/2016/03/18/fast-search-using-postgresql-trigram-indexes/'
+  end
+
+end
--- app/helpers/player_helper.rb
+++ app/helpers/player_helper.rb
@@ -5,10 +5,4 @@ module PlayerHelper
     "#{player.active_year_begin} - #{player.active_year_end}"
   end
 
-  def link_to_baseball_reference(player, text = "view full stats")
-    return unless player
-
-    url = URI.join(ENV.fetch('BASEBALL_REFERENCE_URL'), player.handle).to_s
-    link_to text, url
-  end
 end
