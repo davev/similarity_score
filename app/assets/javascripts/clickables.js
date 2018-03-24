54b28ffc5eaf7dfe4c02792f744b6eaeb3e89ddd
Dave VanderKloot <dave@1530design.com>
2018-03-23 19:35:03 -0500

    prevent event bubbling on url clicks

--- app/assets/javascripts/clickables.js
+++ app/assets/javascripts/clickables.js
@@ -1,4 +1,6 @@
 $(document).on('click', '.clickable', function(event) {
+  event.preventDefault();
+
   let url = $(this).data('url');
   if (url) {
     if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
